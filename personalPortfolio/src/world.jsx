a38f149172389bff2d5de342dfbee6340fd04527
Jacob <92279567+Oia20@users.noreply.github.com>
2024-03-20 22:03:17 -0500

    Added Grand Exchange model from blender to React Three Fiber Scene

--- personalPortfolio/src/world.jsx
+++ personalPortfolio/src/world.jsx
@@ -1,39 +1,35 @@
-import { useRef, useState } from 'react'
-import { Canvas, useFrame } from '@react-three/fiber'
-import { OrbitControls } from '@react-three/drei'
+import React, { useRef } from 'react';
+import { Canvas, useLoader } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
-function Box(props) {
-  // This reference gives us direct access to the THREE.Mesh object
-  const ref = useRef()
-  // Hold state for hovered and clicked events
-  const [hovered, hover] = useState(false)
-  const [clicked, click] = useState(false)
-  // Subscribe this component to the render-loop, rotate the mesh every frame
-  useFrame((state, delta) => (ref.current.rotation.x += delta))
-  // Return the view, these are regular Threejs elements expressed in JSX
+function GrandExchange() {
+  const gltf = useLoader(GLTFLoader, '/models/osrsGE.glb');
+  const group = useRef();
+
   return (
-    <mesh
-      {...props}
-      ref={ref}
-      scale={clicked ? 1.5 : 1}
-      onClick={(event) => click(!clicked)}
-      onPointerOver={(event) => (event.stopPropagation(), hover(true))}
-      onPointerOut={(event) => hover(false)}>
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
-    </mesh>
-  )
+    <group ref={group}>
+      {gltf.scene && <primitive object={gltf.scene} />}
+    </group>
+  );
 }
+
 
 export default function App() {
   return (
-    <Canvas>
-      <ambientLight intensity={Math.PI / 2} />
-      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
-      <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
-      <Box position={[-1.2, 0, 0]} />
-      <Box position={[1.2, 0, 0]} />
+    <Canvas style={{ position: 'absolute', top: 0, left: 0, right: 0, bottom: 0 }} camera={{ position: [-7, 10, 5] }}>
+      <ambientLight intensity={1} />
+      <directionalLight position={[0, 0, 5]} />
+      <pointLight position={[10, 10, 10]} />
+      <mesh  position={[10, 10, 10]}>
+        <boxGeometry/>
+        <meshStandardMaterial />
+      </mesh>
+      {/* Add your model component */}
+      <GrandExchange />
+
+      {/* OrbitControls */}
       <OrbitControls />
     </Canvas>
-  )
+  );
 }
