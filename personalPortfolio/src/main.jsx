ad0c45cd0cc167f018742e9b4c336226a4e69d14
Jacob <92279567+Oia20@users.noreply.github.com>
2024-03-27 21:05:15 -0500

    After receiving some feedback I've decided to entirely rework the site. Changing theme, and design. Rework will be much easier because of how much I've learned.

--- personalPortfolio/src/main.jsx
+++ personalPortfolio/src/main.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import App from './world.jsx'
 import UI from './ui.jsx'
-import Hats from './hats.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
-  <App />
-  {/* <Hats /> */}
   <UI />
   </>
 )
